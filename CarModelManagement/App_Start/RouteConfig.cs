778f0c1fcc0c46bd7bc7f3a0849eb623f6b591c5
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-10-27 23:55:30 +0530

    fd

--- CarModelManagement/App_Start/RouteConfig.cs
+++ CarModelManagement/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace CarModelManagement
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            ); ;
         }
     }
 }
